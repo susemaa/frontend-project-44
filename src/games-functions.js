b7b75d5179ce60615e62bab0db721aaa83f559bb
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 00:39:06 +0300

    moved gamaes-functions.js to src/

---
+++ src/games-functions.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const getAnsw = () => { 
+    const answ = readlineSync.question('Your answer: ');
+
+    return answ;
+}
+
+//returns random value up to 100
+const getRand = () => Math.round(Math.random() * 100);
+
+export { getAnsw, getRand };
