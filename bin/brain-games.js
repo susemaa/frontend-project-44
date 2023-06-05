7218818b8c131c4b784805ac3df60e5d5cf1663c
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-05 23:29:58 +0300

    modified brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 import { getName } from '../src/cli.js'
 
 console.log('Welcome to the Brain Games!');
 
 const playerName = getName();
 console.log(`Hello, ${playerName}!`);
-
