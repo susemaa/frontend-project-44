678cceb698ed0af8d76b2b2f2e40b346fe92f6d6
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 00:41:31 +0300

    modified path to games-functions.js in brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { getName } from '../src/cli.js'
-import { getAnsw, getRand } from './games-functions.js';
+import { getAnsw, getRand } from '../src/games-functions.js';
 
 const playerName = getName();
 console.log(`Hello, ${playerName}!`);
