8a92b014df196276e40614d84f8e2e58e059013d
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 22:02:32 +0300

    fixed prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,8 +3,9 @@ import { getAnsw, checkAnswer } from "../games-functions.js";
 
 const playPrime = () => {
     let counter = 0;
+    console.log('Answer "yes" if the number is prime, otherwise answer "no".');
+    
     while (counter < 3 && counter !== -1) {
-        console.log('Answer "yes" if the number is prime, otherwise answer "no".');
 
         const questionNum = getRand(25);
         console.log(`Question: ${questionNum}`);
