759dd0c4f92956710e5e7420dcbb5cc70bb71810
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 22:18:27 +0300

    fixed even.js

--- src/games/even.js
+++ src/games/even.js
@@ -5,15 +5,15 @@ const playEven = () => {
     let counter = 0;
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-        while (counter < 3 && counter !== -1) {
-            const questionNum = getRand();
-            console.log(`Question: ${questionNum}`);
+    while (counter < 3 && counter !== -1) {
+        const questionNum = getRand();
+        console.log(`Question: ${questionNum}`);
             
-            const corrAnsw = questionNum % 2 === 0 ? 'yes': 'no';
-            const playerAnsw = getAnsw();
+        const corrAnsw = questionNum % 2 === 0 ? 'yes': 'no';
+        const playerAnsw = getAnsw();
             
-            counter = checkAnswer(counter, playerAnsw, corrAnsw);
-        }
+        counter = checkAnswer(counter, playerAnsw, corrAnsw);
+    }
 
     return counter;
 };
