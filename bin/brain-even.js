ec43442c57cd8eaca409bb05a44778f24bdbfaab
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 13:44:08 +0300

    modified brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { getName } from '../src/cli.js'
-import { getAnsw, getRand } from '../src/games-functions.js';
+import { getAnsw, getRand, checkAnswer, isWin } from '../src/games-functions.js';
 
 const playerName = getName();
 
@@ -12,18 +12,11 @@ while (counter < 3 && counter !== -1) {
     console.log(`Question: ${questionNum}`);
 
     const corrAnsw = questionNum % 2 === 0 ? 'yes': 'no';
-    const userAnsw = getAnsw();
+    const playerAnsw = getAnsw();
 
-    if (userAnsw === corrAnsw) {
-        counter += 1;
-        console.log('Correct!');
-    } else {
-        counter = -1;
-        console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${corrAnsw}'`);
-        console.log(`Let's try again, ${playerName}!`);
-    }
+    counter = checkAnswer(counter, playerAnsw, corrAnsw);
 }
 
-if (counter > 0) {
+if (counter > 0) { 
     console.log(`Congratulations, ${playerName}!`);
 }
