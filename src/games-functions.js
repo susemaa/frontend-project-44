307999e9437780b066a01c322f8c37ad51047ee1
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-06 13:31:58 +0300

    added new funcitions to games-functions.js

--- src/games-functions.js
+++ src/games-functions.js
@@ -6,7 +6,57 @@ const getAnsw = () => {
     return answ;
 }
 
-//returns random value up to 100
-const getRand = () => Math.round(Math.random() * 100);
+const getRand = (maxValue = 100) => {
+    return Math.floor(Math.random() * maxValue); // < maxValue
+}
 
-export { getAnsw, getRand };
+const getOperation = () => {
+    const operationNum = getRand(3);
+    switch (operationNum) {
+        case 0:
+            return '+';
+        case 1:
+            return '-';
+        case 2:
+            return '*';
+    }
+};
+
+const getExpression = () => {
+    const [v1, v2] = [getRand(15), getRand(15)];
+    const operation = getOperation();
+    const expressionStr = `${v1}${operation}${v2}`;
+
+    switch (operation) {
+        case '+':
+            return [expressionStr, v1 + v2];
+        case '-':
+            return [expressionStr, v1 - v2];
+        case '*':
+            return [expressionStr, v1 * v2];
+    }
+};
+
+const checkAnswer = (counter, playerAnsw, corrAnsw) => {
+    if (playerAnsw === corrAnsw) {
+        counter += 1;
+        console.log('Correct!');
+    } else {
+        counter = -1;
+        console.log(`'${playerAnsw}' is wrong answer ;(. Correct answer was '${corrAnsw}'`);
+        console.log(`Let's try again, ${playerName}!`);
+    }
+
+    return counter;
+}
+
+const isWin = (counter, playerName) => {
+    if (counter > 0) {
+        console.log(`Congratulations, ${playerName}!`);
+        return true;
+    }
+
+    return false;
+}
+
+export { getAnsw, getRand, getExpression, checkAnswer, isWin };
