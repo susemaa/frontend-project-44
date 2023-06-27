86e2fdd85935cf132c274570011a0e9a93e22742
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-28 00:18:23 +0300

    fixed calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,19 +1,30 @@
 import playGame from '../index.js';
 import getRandomValue from '../utils.js';
 
-const getExpression = () => {
-  const [value1, value2] = [getRandomValue(0, 15), getRandomValue(0, 15)];
-  const operationNum = getRandomValue(0, 2);
-
-  switch (operationNum) {
+const getRandomOperator = () => {
+  const operatorNum = getRandomValue(0, 2);
+  switch (operatorNum) {
     case 0:
-      return [`${value1} + ${value2}`, value1 + value2];
+      return '+';
     case 1:
-      return [`${value1} - ${value2}`, value1 - value2];
+      return '-';
     case 2:
-      return [`${value1} * ${value2}`, value1 * value2];
+      return '*';
     default:
       throw new Error('Unknown operationNum!');
+  }
+};
+
+const getCorrectAnswer = (number1, number2, operator) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error('Unknown operator!');
   }
 };
 
@@ -21,9 +32,11 @@ const playCalc = () => {
   const message = 'What is the result of the expression?';
 
   const getQnA = () => {
-    const [expression, questionValue] = getExpression();
-    const question = `Question: ${expression}`;
-    const answer = String(questionValue);
+    const randomNumber1 = getRandomValue(1, 30);
+    const randomNumber2 = getRandomValue(1, 30);
+    const randomOperator = getRandomOperator();
+    const question = `Question: ${randomNumber1} ${randomOperator} ${randomNumber2}`;
+    const answer = String(getCorrectAnswer(randomNumber1, randomNumber2, randomOperator));
 
     return [question, answer];
   };
