0a58da25d2b7972f18c8a70d846f65650b70d131
Arseny Radoslavov <rad.archeg@gmail.com>
2023-06-27 23:41:13 +0300

    added isEven in even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,8 @@
 import playGame from '../index.js';
 import getRandomValue from '../utils.js';
 
+const isEven = (number) => number % 2 === 0;
+
 export default () => {
   const message = 'Answer "yes" if the number is even, otherwise answer "no".';
   const questions = [];
@@ -8,7 +10,7 @@ export default () => {
   for (let i = 0; i < 3; i += 1) {
     const questionNum = getRandomValue(0, 100);
     questions[i] = `Question: ${questionNum}`;
-    answers[i] = questionNum % 2 === 0 ? 'yes' : 'no';
+    answers[i] = isEven(questionNum) ? 'yes' : 'no';
   }
 
   playGame(message, questions, answers);
